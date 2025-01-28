86a5274d63e4e068c42522e626749f7722c21644
Miguel Palacio <migpalg@gmail.com>
2025-01-28 16:07:38 -0500

    feat: optimized output size with manual chunks

--- src/features/auth/screens/login-screen.tsx
+++ src/features/auth/screens/login-screen.tsx
@@ -1,7 +1,25 @@
+import { Button, TextField } from "@mui/material";
+import { useTranslation } from "react-i18next";
+
 export const LoginScreen = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <h2>Hello world</h2>
+      <TextField
+        variant="outlined"
+        label={t("auth.login.email.label")}
+        fullWidth
+      />
+      <TextField
+        variant="outlined"
+        label={t("auth.login.password.label")}
+        type="password"
+        fullWidth
+      />
+      <Button variant="contained" color="primary" fullWidth>
+        {t("auth.login.submit")}
+      </Button>
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        // Optimize the build by splitting the vendor chunk
+        manualChunks: {
+          react: ["react"],
+          "react-dom": ["react-dom"],
+          "react-router": ["react-router"],
+          i18next: ["i18next"],
+          "react-i18next": ["react-i18next"],
+          "@mui/material": ["@mui/material"],
+          "@mui/icons-material": ["@mui/icons-material"],
+        },
+      },
+    },
+  },
+});
