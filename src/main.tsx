a62f6e4c734ac41962dd8a1fe7a742f030db5939
Miguel Palacio <migpalg@gmail.com>
2025-01-28 15:31:07 -0500

    feat(core): added dom router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./features/auth/contexts/auth-provider";
+import { Router } from "./features/core/router";
 import { auth } from "./features/auth/firebase";
-import { App } from "./features/core/app";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider auth={auth}>
-      <App />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </AuthProvider>
   </StrictMode>
 );
