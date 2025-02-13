58dc1c560229c954d0a03a4bb465e8a6d9d4d8da
Miguel Palacio <migpalg@gmail.com>
2025-02-13 12:31:41 -0500

    feat: added spanish translations

--- src/features/core/i18n.ts
+++ src/features/core/i18n.ts
@@ -1,12 +1,19 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi, { HttpBackendOptions } from "i18next-http-backend";
 
-const options: HttpBackendOptions = {
+const backendOptions: HttpBackendOptions = {
   loadPath: "/locales/{{lng}}.json",
 };
 
-i18n.use(initReactI18next).use(HttpApi).init({
-  lng: "en",
-  backend: options,
-});
+i18n
+  // Initialize i18next plugins and confiugurations
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ["en", "es"],
+    fallbackLng: "en",
+    backend: backendOptions,
+  });
