027f696f795904a35b4053eef6e68675f4e46793
Miguel Palacio <migpalg@gmail.com>
2025-02-18 22:22:28 -0500

    feat(dashboard): added scaffold for setting up routine

--- src/features/dashboard/screens/home-screen.tsx
+++ src/features/dashboard/screens/home-screen.tsx
@@ -1,6 +1,15 @@
 import { useTranslation } from "react-i18next";
 import { useAuth } from "../../auth/hooks/use-auth";
-import { Avatar, Box, Button, Typography } from "@mui/material";
+import { Avatar, Box, Button, Container, Paper, styled, Typography } from "@mui/material";
+
+import DumbbellIllustration from "../assets/dumbbell.svg";
+
+const HeaderImage = styled("img")`
+  position: absolute;
+  inset: 0;
+  object-fit: contain;
+  transform: scale(1.75) rotate(-20deg);
+`;
 
 export const HomeScreen = () => {
   const { signOut, user } = useAuth();
@@ -14,7 +23,7 @@ export const HomeScreen = () => {
   };
 
   return (
-    <Box sx={{ padding: 2 }}>
+    <Container sx={{ overflow: "hidden", py: 2 }}> 
       <Box
         sx={{
           display: "flex",
@@ -32,9 +41,46 @@ export const HomeScreen = () => {
           </Typography>
         </Box>
       </Box>
+
+      <Paper
+        elevation={2}
+        sx={(theme) => ({
+          p: 2,
+          my: 2,
+          borderRadius: 4,
+          backgroundColor: theme.palette.grey[100],
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          gap: 4,
+        })}
+      >
+        <Box>
+          <Typography
+            sx={{ lineHeight: 1.1, mb: 0.5 }}
+            variant="h6"
+            fontWeight={800}
+          >
+            Setup your workout plan
+          </Typography>
+          <Typography sx={{ opacity: 0.5 }} variant="subtitle2">
+            Workout
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            width: 100,
+            height: 70,
+            position: "relative",
+          }}
+        >
+          <HeaderImage src={DumbbellIllustration} alt="Dumbbell illustration" />
+        </Box>
+      </Paper>
+
       <Button variant="contained" onClick={handleSignOut} color="error">
         Sign out
       </Button>
-    </Box>
+    </Container>
   );
 };
