4adfa3afd486c828de53fb05e4f44bfef2848565
Miguel Palacio <migpalg@gmail.com>
2025-02-21 00:21:00 -0500

    feat(auth): added db to the firebase config

--- src/features/auth/firebase.ts
+++ src/features/auth/firebase.ts
@@ -1,12 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { config } from "./config";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 
 export const app = initializeApp(config.firebase);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 
 if (import.meta.env.DEV) {
   connectAuthEmulator(auth, import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_URL, {
     disableWarnings: true,
   });
+
+  connectFirestoreEmulator(
+    db,
+    import.meta.env.VITE_FIREBASE_FIRESTORE_EMULATOR_URL,
+    import.meta.env.VITE_FIREBASE_FIRESTORE_EMULATOR_PORT,
+  );
 }
